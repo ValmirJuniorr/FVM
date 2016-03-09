f4dc1b686476879763110eaecfe9c666146cd414
ValmirJuniorr <valmir_junyor@hotmail.com>
2016-03-08 22:51:31 -0300

    configuração da jpa

---
+++ source/web/FVM/src/fvm/model/Main.java
@@ -0,0 +1,14 @@
+package fvm.model;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("FVM");
+		factory.close();
+
+	}
+
+}
--- source/web/FVM/src/fvm/model/Produto.java
+++ source/web/FVM/src/fvm/model/Produto.java
@@ -1,9 +1,27 @@
 package fvm.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Produtos")
 public class Produto {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="Id_Produto")
 	private int id;
+	
+	@Column(name="Nome_Produto")
 	private String name;
+	
+	@Column(name="Descricao")
 	private String description;
+	
 	public int getId() {
 		return id;
 	}
