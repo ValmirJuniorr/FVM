0153e757ebf97f185decb1ef994d248aa01e3ae4
ValmirJuniorr <valmir_junyor@hotmail.com>
2016-05-16 11:59:53 -0300

    HelloWord Android adcionado

---
+++ source/mobile/FVM/app/src/main/java/fvm/com/br/fvm/MainActivity.java
@@ -0,0 +1,13 @@
+package fvm.com.br.fvm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
