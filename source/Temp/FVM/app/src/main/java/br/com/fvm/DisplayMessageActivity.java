7c04fba6b88a1c24b69e8c1e72b55bdb63ba45af
Valmir Junior <valmir_junyor@hotmail.com>
2016-05-18 23:12:18 -0300

    Adção do projeto temporario seguindo o passo a passo De Pablo

--- source/Temp/FVM/app/src/main/java/br/com/fvm/DisplayMessageActivity.java
+++ source/Temp/FVM/app/src/main/java/br/com/fvm/DisplayMessageActivity.java
@@ -1,6 +1,8 @@
 package br.com.fvm;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,8 +12,14 @@ public class DisplayMessageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPref =getSharedPreferences(MainActivity.BR_COM_FVM_PREFERENCES, Context.MODE_PRIVATE);
+        long highScore = sharedPref.getInt(MainActivity.BR_COM_FVM_NUMBER, 0);
+
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        message+=highScore;
         TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(message);
--- source/Temp/FVM/app/src/main/java/br/com/fvm/MainActivity.java
+++ source/Temp/FVM/app/src/main/java/br/com/fvm/MainActivity.java
@@ -1,6 +1,8 @@
 package br.com.fvm;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,8 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "br.com.fvm.MESSAGE";
+    public final static String BR_COM_FVM_PREFERENCES= "br.com.fvm.preferences";
+    public final static String BR_COM_FVM_NUMBER = "br.com.fvm.number";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
+
+        SharedPreferences preferences = this.getSharedPreferences(BR_COM_FVM_PREFERENCES,Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(BR_COM_FVM_NUMBER, 120);
+        editor.commit();
+
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
