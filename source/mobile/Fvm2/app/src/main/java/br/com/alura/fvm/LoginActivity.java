ff9ff47a0390af163f78fdf44761cab64039bb72
Valmir Junior <valmir_junyor@hotmail.com>
2016-06-22 20:35:29 -0300

    MEsclagem das apps

--- source/mobile/Fvm2/app/src/main/java/br/com/alura/fvm/LoginActivity.java
+++ source/mobile/Fvm2/app/src/main/java/br/com/alura/fvm/LoginActivity.java
@@ -1,6 +1,10 @@
 package br.com.alura.fvm;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,42 +13,97 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
 
 import java.util.Objects;
 
-public class LoginActivity extends AppCompatActivity {
+import br.com.alura.fvm.modelo.User;
+import br.com.alura.fvm.util.HTTPUtils;
 
-    private String nome;
-    private String minhaSenha;
+public class LoginActivity extends AppCompatActivity {
+    EditText editLogin,editSenha;
+
+    private String nome,senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText login= (EditText) findViewById(R.id.id_login);
-        EditText senha= (EditText) findViewById(R.id.id_senha);
-
-
-
-        nome= login.getText().toString();
-        minhaSenha= senha.getText().toString();
-        Button botao= (Button) findViewById(R.id.botao_login);
-        botao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    Toast.makeText(LoginActivity.this, "clikei", Toast.LENGTH_LONG).show();
-               if ((nome.equals("kleber")&& minhaSenha.equals("123"))) {
-                    Toast.makeText(LoginActivity.this,"indo pra lista", Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(LoginActivity.this, ListaProdutosActivity.class);
-                    startActivity(intent);
-               }else{
-                   Toast.makeText(LoginActivity.this,"Não entrou", Toast.LENGTH_LONG).show();
-               }
-            }
-        });
-
+        editLogin= (EditText) findViewById(R.id.id_login);
+        editSenha= (EditText) findViewById(R.id.id_senha);
     }
+
+    public void logar (View view){
+        nome= editLogin.getText().toString();
+        senha= editSenha.getText().toString();
+        new LoginTask().execute(nome,senha);
+
+//        Toast.makeText(LoginActivity.this, "clikei", Toast.LENGTH_LONG).show();
+//        if ((nome.equals("kleber")&& senha.equals("123"))) {
+//            Toast.makeText(LoginActivity.this,"indo pra lista", Toast.LENGTH_LONG).show();
+//            Intent intent = new Intent(LoginActivity.this, ListaProdutosActivity.class);
+//            startActivity(intent);
+//        }else{
+//            Toast.makeText(LoginActivity.this,"Não entrou", Toast.LENGTH_LONG).show();
+//        }
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, String[]> {
+        ProgressDialog dialog;
+        @Override
+        protected void onPreExecute() {
+            dialog = new ProgressDialog(LoginActivity.this);
+            dialog.show();
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            try {
+                String login,password;
+                login=params[0];
+                password=params[1];
+
+                String url = "http://192.168.43.194:8080/FVM/service/user/login" ;
+                Gson gson=new Gson();
+                User user=new User();
+                user.setLogin(login);
+                user.setPassword(password);
+                String data=gson.toJson(user);
+                String conteudo= HTTPUtils.requestPost(url,data);
+                if(conteudo.equals("Login successful")){
+                    return new String[]{"ok"};
+                }
+                return  new String[]{"opa"};
+            } catch (Exception e) {
+                return new String[]{e.getMessage()};
+            }
+
+        }
+
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            Intent intent = new Intent(LoginActivity.this, ListaProdutosActivity.class);
+            startActivity(intent);
+//            if(result[0].equals("ok")){
+//                Intent intent = new Intent(LoginActivity.this, ListaProdutosActivity.class);
+//                startActivity(intent);
+//
+//            }else{
+//                Dialog alert=new Dialog(LoginActivity.this);
+//                alert.setTitle("Erro no Login");
+//
+//            }
+
+            dialog.dismiss();
+
+
+        }
+    }
+
 
 }
---
+++ source/mobile/Fvm2/app/src/main/java/br/com/alura/fvm/modelo/User.java
@@ -0,0 +1,56 @@
+package br.com.alura.fvm.modelo;
+
+public class User {
+	
+		
+	private int Id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String login;
+	
+	private String password;
+	
+	
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "User [Id=" + Id + ", name=" + name + ", email=" + email + ", login=" + login + ", password=" + password
+				+ "]";
+	}
+	
+	
+
+}
